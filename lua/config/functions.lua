aecf4873626f573915a56229e0ccd0366f91c1f8
Simeon Vutov <simeon32.dev@gmail.com>
2026-01-02 17:17:58 +0200

    A quick fix on the variables we update, so we can copy from nvim. There was a problem with WAYLAND_DISPLAY and that wayland-1 might not be the correct value in the variable WAYLAND_DISPLAY

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -2,19 +2,19 @@ local M = {}
 
 M.update_tmux_env = function()
   if vim.env.TMUX then
-    local vars = { "DISPLAY", "WAYLAND_DISPLAY", "XAUTHORITY" }
+    local vars = { "DISPLAY", "WAYLAND_DISPLAY", "XAUTHORITY", "XDG_RUNTIME_DIR" }
     for _, var in ipairs(vars) do
-      local handle = io.popen("tmux show-env -s " .. var)
+      local handle = io.popen("tmux show-env -s " .. var .. " 2>/dev/null")
       if handle then
         local result = handle:read("*a")
         handle:close()
-        local value = result:match(var .. '=(.-);')
-        if value then
+        local value = result:match(var .. '="(.-)"') or result:match(var .. '=(.-);')
+        if value and value ~= "" then
           vim.env[var] = value
         end
       end
     end
-    vim.notify("Tmux environment synced", vim.log.levels.INFO)
+    vim.cmd('runtime autoload/provider/clipboard.vim')
   end
 end
 
